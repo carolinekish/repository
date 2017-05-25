480aac87fdb1494aa5199c6ffdd3aeaf179a112f
Caroline Kish <ckish3@gatech.edu>
2017-05-24 23:00:18 -0400

    wrote Person1.java calc method

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -3,8 +3,8 @@ package edu.gatech.oad.antlab.person;
 /**
  *  A simple class for person 1
  *  returns their name and a
- *  modified string 
- *  
+ *  modified string
+ *
  *  @author Bob
  *  @version 1.1
  */
@@ -30,16 +30,32 @@ public class Person1 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 1 put your implementation here
-	  return null;
+        if (input.length() <= 1) {
+            return input;
+        }
+
+        char[] in = input.toCharArray();
+        int begin = 0;
+
+        char temp1 = in[0];
+        char temp2 = in[1];
+
+        for (int i = 2; i < input.length(); i++) {
+            in[i - 2] = in[i];
+        }
+        in[input.length() - 2] = temp1;
+        in[input.length() -1] = temp2;
+
+        String output = String.valueOf(in);
+        return output;
 	}
-	
+
 	/**
 	 * Return a string rep of this object
 	 * that varies with an input string
 	 *
 	 * @param input the varying string
-	 * @return the string representing the 
+	 * @return the string representing the
 	 *         object
 	 */
 	public String toString(String input) {
