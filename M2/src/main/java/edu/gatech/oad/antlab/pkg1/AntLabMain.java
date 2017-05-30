da78384f192a27b86453d9df4566594516203b86
Kipp Morris <kipp.morris@gmail.com>
2017-05-30 12:36:51 -0400

    Fixed mistake I made in AntLabMain when I accidentally made myself Person2

--- M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -61,8 +61,8 @@ import edu.gatech.oad.antlab.pkg2.*;
 		  toPrint += p1.toString("ckish3");
 		  //Person2 replace P2 with your name
 		  //and gburdell with your gt id
-		  Person2 p2 = new Person2("Kipp Morris");
-		  toPrint += p2.toString("kmorris9");
+		  Person2 p2 = new Person2("P2");
+		  toPrint += p2.toString("gburdell5");
 		  //Person3 replace P3 with your name
 		  //and gburdell3 with your gt id
 		  Person3 p3 = new Person3("David Wang");
@@ -73,8 +73,8 @@ import edu.gatech.oad.antlab.pkg2.*;
           toPrint += p4.toString("glin39");
                   //Person5 replace P4 with your name
                   //and gburdell5 with your gt id
-                  Person5 p5 = new Person5("P5");
-                  toPrint += p5.toString("gburdell5");
+                  Person5 p5 = new Person5("Kipp Morris");
+                  toPrint += p5.toString("kmorris9");
 
         System.out.println(toPrint);
 
