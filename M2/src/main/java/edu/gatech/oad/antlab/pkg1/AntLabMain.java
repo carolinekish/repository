850fa6e3bb1233026170dc467e79dada4994764b
Caroline Kish <caroline.rose.kish@gmail.com>
2017-06-01 15:16:59 -0400

    fixed antlabmain

--- M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -84,6 +84,8 @@ import edu.gatech.oad.antlab.pkg2.*;
 
     /**
      * entry point for the program
+     * @param args the default argument for main method
+     * in java
      */
      public static void main(String[] args) {
 
